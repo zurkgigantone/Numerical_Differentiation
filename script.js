a0c5bf07454c428c67b3c440c032ec8c23c0cac9
zurkgigantone <135961426+zurkgigantone@users.noreply.github.com>
2023-06-08 17:04:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+function computeFiniteDifferenceDerivative(f, xi, h, order, method) {
+    const parser = math.parser();
+    parser.evaluate('f(x) = ' + f);
+  
+    let derivative = 0;
+    for (let i = 0; i < order; i++) {
+      if (method === 'forward') {
+        derivative = (parser.evaluate('f(' + (xi + h) + ')') - parser.evaluate('f(' + xi + ')')) / h;
+      } else if (method === 'backward') {
+        derivative = (parser.evaluate('f(' + xi + ')') - parser.evaluate('f(' + (xi - h) + ')')) / h;
+      } else if (method === 'centered') {
+        derivative = (parser.evaluate('f(' + (xi + h) + ')') - parser.evaluate('f(' + (xi - h) + ')')) / (2 * h);
+      }
+      xi += h;
+    }
+  
+    return derivative;
+  }
+  
+  function computeDerivative(order) {
+    const functionInput = document.getElementById('functionInput').value;
+    const xInput = parseFloat(document.getElementById('xInput').value);
+    const hInput = parseFloat(document.getElementById('hInput').value);
+    const methodInput = document.getElementById('methodInput').value;
+  
+    clearTable();
+  
+    const derivative = computeFiniteDifferenceDerivative(functionInput, xInput, hInput, order, methodInput);
+  
+    const resultsTable = document.getElementById('resultsTable');
+    const newRow = resultsTable.insertRow();
+    newRow.innerHTML = `
+      <td>${getDerivativeName(order)}</td>
+      <td>${derivative}</td>
+    `;
+  }
+  
+  function computeAllDerivatives() {
+    const functionInput = document.getElementById('functionInput').value;
+    const xInput = parseFloat(document.getElementById('xInput').value);
+    const hInput = parseFloat(document.getElementById('hInput').value);
+    const methodInput = document.getElementById('methodInput').value;
+  
+    clearTable();
+  
+    const resultsTable = document.getElementById('resultsTable');
+    resultsTable.innerHTML = `
+      <tr>
+        <th>Derivative</th>
+        <th>Value</th>
+      </tr>
+    `;
+  
+    for (let i = 1; i <= 4; i++) {
+      const derivative = computeFiniteDifferenceDerivative(functionInput, xInput, hInput, i, methodInput);
+  
+      const newRow = resultsTable.insertRow();
+      newRow.innerHTML = `
+        <td>${getDerivativeName(i)}</td>
+        <td>${derivative}</td>
+      `;
+    }
+  }
+  
+  function clearTable() {
+    const resultsTable = document.getElementById('resultsTable');
+    resultsTable.innerHTML = `
+      <tr>
+        <th>Derivative</th>
+        <th>Value</th>
+      </tr>
+    `;
+  }
+  
+  function getDerivativeName(order) {
+    return order === 1 ? 'First Derivative' :
+           order === 2 ? 'Second Derivative' :
+           order === 3 ? 'Third Derivative' :
+           order === 4 ? 'Fourth Derivative' :
+           '';
+  }
+  
